01e4cff604e732092ac06cfe9c599f0f084ff0d0
haokeliu <haokeliu@gmail.com>
2020-12-13 21:49:24 +0800

    init project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <el-container>
+            <el-header class="homeHeader">
+                <div class="title">微人事</div>
+                <el-dropdown class="userInfo" @command="commandHandler">
+                    <span class="el-dropdown-link">
+                        <i><img :src="user.userface" alt="">{{user.name}}</i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item command="userinfo">个人中心</el-dropdown-item>
+                        <el-dropdown-item command="setting">设置</el-dropdown-item>
+                        <el-dropdown-item  command="logout" divided>注销登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <el-menu @select="menuClick">
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+                            <el-menu-item index="1-1">选项1</el-menu-item>
+                            <el-menu-item index="1-2">选项2</el-menu-item>
+                        </el-submenu>
+                    </el-menu>
+                </el-aside>
+                <el-container>
+                    <el-main>Main</el-main>
+                    <el-footer>Footer</el-footer>
+                </el-container>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import {getRequest} from "../util/api";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "Home",
+        data() {
+            return{
+                user:JSON.parse(window.sessionStorage.getItem('user'))
+            }
+        },
+        methods: {
+            commandHandler(cmd) {
+                if (cmd === 'logout') {
+                    this.$confirm('此操作将注销登录, 是否继续?', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }).then(() => {
+                        getRequest("/logout");
+                        window.sessionStorage.removeItem('user')
+                        this.$router.replace('/')
+                    }).catch(() => {
+                        this.$message({
+                            type: 'info',
+                            message: '已取消操作'
+                        });
+                    });
+                }
+            },
+            menuClick(index, indexPath){
+                console.log(index, indexPath)
+            }
+        }
+    }
 </script>
+
+<style scoped>
+    .homeHeader {
+        background-color: #409eff;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0px 15px;
+        box-sizing: border-box;
+    }
+    .title {
+        font-size: 30px;
+        font-family: 华文行楷;
+        color: #ffffff;
+    }
+    .userInfo {
+        cursor: pointer;
+    }
+    .el-dropdown-link img {
+        width: 48px;
+        height: 48px;
+        border-radius: 24px;
+        margin-left: 8px;
+    }
+    .el-dropdown-link {
+        display: flex;
+        align-items: center;
+    }
+</style>
